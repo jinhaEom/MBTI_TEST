74a49eb4bee04be82a8717a8a010d2ae522e7b3b
Eom Jinha <84216838+jinhaEom@users.noreply.github.com>
2021-10-05 16:26:42 +0900

    Add files via upload

--- start/js/start.js
+++ start/js/start.js
@@ -2,6 +2,29 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 // const 는 변수를 상수로 만들어줌. var 도 사용 할 수 있음 ,
 //querySelecter 는 문서에서 css 선택자에 대응되는 값을 선택해주는 기능.
+function addAnswer(answerText, qIdx){
+  var a = document.querySelector('.answerBox');
+  var answer = document.createElement('button');
+  answer.classList.add('answerList');
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+  answer.addEventListener("click", function(){
+    var children = document.querySelectorAll('.answerList');
+    for(let i = 0; i <children.length; i++){
+      children[i].disabled= true;
+      children[i].style.display='none';
+    }
+    goNext(++qIdx);
+  }, false);
+}
+function goNext(qIdx){
+  var q = document.querySelector('.qBox');
+  q.innerHTML = qnaList[qIdx].q;
+  for(let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+  }
+}
+
 function begin(){
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
@@ -12,5 +35,7 @@ function begin(){
       main.style.display = "none";
       qna.style.display = "block"
     }, 450)
+    let qIdx = 0;
+    goNext(qIdx);
   }, 450);
 }
